068162bf6277bc33eea0897487341f670d9d8880
Kelly Holtzman <holtzmak@uregina.ca>
2017-10-26 17:21:41 -0600

    Updated to include list of Orders, and constructors

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -1,17 +1,30 @@
 package Customer;
 
+import java.util.LinkedList;
+
+import Order.Order;
+
 public class Customer {
 	
+	LinkedList<Order> orders = new LinkedList<Order>();
 	private String name;
 	private String address;
 	private float creditRating;
 	private float discountRating;
 	
-	public Customer() {
-		this.name = "null";
-		this.address = "null";
-		this.creditRating = 0;
-		this.discountRating = 0;
+	public Customer(String name, String address, float creditRating, float discountRating) {
+		setName(name);
+		setAddress(address);
+		setCreditRating(creditRating);
+		setDiscountRating(discountRating);
+	}
+	
+	public void createOrder(Order order) {
+		orders.add(order);
+	}
+	
+	public LinkedList<Order> getOrders() {
+		return orders;
 	}
 	
 	public void setName(String name) {
