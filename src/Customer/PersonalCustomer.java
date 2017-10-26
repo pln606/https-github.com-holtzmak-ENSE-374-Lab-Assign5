e31454a55801b666edffee823c7ab8d2acfb5403
Kelly Holtzman <holtzmak@uregina.ca>
2017-10-26 17:23:10 -0600

    Added super constructor for Customer

--- src/Customer/PersonalCustomer.java
+++ src/Customer/PersonalCustomer.java
@@ -4,6 +4,11 @@ public class PersonalCustomer extends Customer {
 	
 	private int creditCardNumber;
 	
+	public PersonalCustomer(int creditCardNumber, String name, String address, float creditRating, float discountRating) {
+		super(name, address, creditRating, discountRating);
+		setCreditCardNumber(creditCardNumber);
+	}
+	
 	public void setCreditCardNumber(int creditCardNumber) {
 		this.creditCardNumber = creditCardNumber;
 	}
