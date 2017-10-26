1f8582da46115f5aa075484cb0ebdaa694d22d50
Kelly Holtzman <holtzmak@uregina.ca>
2017-10-26 17:20:50 -0600

    Updated to include super constructor for Customer

--- src/Customer/CorporateCustomer.java
+++ src/Customer/CorporateCustomer.java
@@ -4,6 +4,11 @@ public class CorporateCustomer extends Customer {
 	
 	private String contactName;
 	
+	public CorporateCustomer(String contactName, String name, String address, float creditRating, float discountRating) {
+		super(name, address, creditRating, discountRating);
+		setContactName(contactName);
+	}
+	
 	public void setContactName(String contactName) {
 		this.contactName = contactName;
 	}
