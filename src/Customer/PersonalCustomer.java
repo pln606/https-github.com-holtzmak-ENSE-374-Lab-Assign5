feb6051f6baddde82ebe00029e4a2a2f4c538652
Kelly Holtzman <holtzmak@uregina.ca>
2017-10-28 11:43:18 -0600

    Updated code documentation

--- src/Customer/PersonalCustomer.java
+++ src/Customer/PersonalCustomer.java
@@ -1,8 +1,28 @@
+/*
+ * PersonalCustomer.java
+ *
+ * DESCRIPTION:
+ * Class PersonalCustomer for Lab Assignment 4
+ *
+ * ENSE 374-092 Lab Assignment 4
+ * 
+ * @author Kelly Holtzman
+ * I.D.: 200366225
+ */
+
 package Customer;
 
 public class PersonalCustomer extends Customer {
+	/* Class PersonalCustomer extends Class Customer
+	 * It holds the variables for a PersonalCustomer
+	 */
 	
 	private int creditCardNumber;
+	
+	/**
+	 * @param creditCardNumber
+	 * 		the PersonalCustomer's credit card number, for payment
+	 **/
 	
 	public PersonalCustomer(int creditCardNumber, String name, String address, float creditRating, float discountRating) {
 		super(name, address, creditRating, discountRating);
