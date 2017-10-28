ac8c67152cb80efa05d5f71c91ebb0a16ac8f551
Kelly Holtzman <holtzmak@uregina.ca>
2017-10-28 11:35:35 -0600

    Updated code documentation

--- src/Customer/CorporateCustomer.java
+++ src/Customer/CorporateCustomer.java
@@ -1,8 +1,29 @@
+/*
+ * CorporateCustomer.java
+ *
+ * DESCRIPTION:
+ * Class CorporateCustomer for Lab Assignment 4
+ *
+ * ENSE 374-092 Lab Assignment 4
+ * 
+ * @author Kelly Holtzman
+ * I.D.: 200366225
+ */
+
 package Customer;
 
 public class CorporateCustomer extends Customer {
+	/* Class CorporateCustomer extends Class Customer
+	 * It holds the variables for a CorporateCustomer
+	 */
 	
 	private String contactName;
+	
+	/**
+	 * @param contactName
+	 * 		the CorporateCustomer's contact name
+	 * 
+	 **/
 	
 	public CorporateCustomer(String contactName, String name, String address, float creditRating, float discountRating) {
 		super(name, address, creditRating, discountRating);
