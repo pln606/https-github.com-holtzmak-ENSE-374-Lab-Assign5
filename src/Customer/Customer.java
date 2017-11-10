34d6600e75f0f96ac85c1bb774fa03b838e9111c
Kelly Holtzman <holtzmak@uregina.ca>
2017-11-10 01:24:56 -0600

    Updated documentation to match assign5

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -2,9 +2,9 @@
  * Customer.java
  *
  * DESCRIPTION:
- * Class Customer for Lab Assignment 4
+ * Class Customer for Lab Assignment 5
  *
- * ENSE 374-092 Lab Assignment 4
+ * ENSE 374-092 Lab Assignment 5
  * 
  * @author Kelly Holtzman
  * I.D.: 200366225
